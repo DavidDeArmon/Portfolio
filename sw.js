75bd29da2232fc988f9096698812faf5d71823bf
David DeArmon <david.dearmon@gmail.com>
2019-01-16 17:19:44 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-09e445b0e73cc51c4372.js"
+    "url": "webpack-runtime-7abd94fde6090443a9ca.js"
   },
   {
     "url": "app-73a1aec92c8932f00dda.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "5a7e6adef3e461a405d7cd60d5291868"
+    "revision": "7ae477355a5231823d36295ac86ed155"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "398e34b9d9f9da4d5274746d6214c6bc"
+    "revision": "af3db28b7799507a049ace59712a35da"
   },
   {
     "url": "1.4f310fcc093245bafcb1.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
     "url": "1-9c366a9491367b1d4c4e.js"
   },
   {
-    "url": "component---src-pages-index-js-fecb801679b1c914845a.js"
+    "url": "component---src-pages-index-js-2d667bd0824e5a0b89f6.js"
   },
   {
     "url": "0-cb83a8b81a4f665b9ddc.js"
